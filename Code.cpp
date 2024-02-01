a59a6c8ef988ef00969da9f5b3dd16bc4ae52b29
Albert Joseph <albertjoseph10000@gmail.com>
2024-02-01 21:51:51 +0530

    The Initial Commit

---
+++ Code.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int n;
+  cin >> n;
+  cout << "The Entered Number is : " << n << endl;
+
+  cout << "Newly Added Line 1" << endl;
+  cout << "Newly Added Line 2" << endl;
+
+  cout << "Another Developer modified the file" << endl;
+
+  cout << "Another Developer modified the file" << endl;
+
+  cout << "Finally Add" << endl;
+  cout << "Finally Add1 << endl;
+
+}
