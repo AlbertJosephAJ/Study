b1cc5ea9ed0067ce5f50d304677efb1ad0ce26ff
Albert Joseph <albertjoseph10000@gmail.com>
2024-02-01 21:19:58 +0530

    Commit in Local

--- Code.cpp
+++ Code.cpp
@@ -5,6 +5,10 @@ int main() {
   int n;
   cin >> n;
   cout << "The Entered Number is : " << n << endl;
+<<<<<<< HEAD
   cout << "Newly Added Line 1" << endl;
   cout << "Newly Added Line 2" << endl;
+=======
+  cout << "Another Developer modified the file" << endl;
+>>>>>>> 3e2f470995acc034e9dc4357912af2a211c913cb
 }
